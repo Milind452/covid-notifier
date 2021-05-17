c307c1b16003ea09e8c49f07e651930ad24e4be5
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-05-17 21:08:58 +0530

    Get basic covid data

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ from bs4 import BeautifulSoup
 def getSoupObject(url):
     return BeautifulSoup(requests.get(url).content, 'html.parser')
 
+def getGeneralData(soup):
+    dataList = soup.find('div', class_ = 'col-xs-8 site-stats-count').findAll('li')
+    dataDict = dict()
+    for li in dataList:
+        tmp = li.text.strip().split()
+        dataDict[tmp[0]] = tmp[1]
+    print(dataDict)
+
 if __name__ == '__main__':
     print('__Start')
-    url = 'https://www.github.com'
+    url = 'https://www.mohfw.gov.in/'
     soup = getSoupObject(url)
-    print(soup.title)
+    getGeneralData(soup)
     print('__End')
