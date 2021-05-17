f2eb608d3393cc653b9606ee1f2819ae4bd78f8d
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-05-17 23:26:50 +0530

    Get state wise covid data

--- main.py
+++ main.py
@@ -15,9 +15,20 @@ def getGeneralData(soup):
     dataDict[vaccineData[1]] = "".join(vaccineData[3].split(','))
     print(dataDict)
 
+def getStateData(soup):
+    table = soup.find('table', class_ = 'table table-striped table-bordered').find('tbody')
+    stateData = dict()
+    for row in table.findAll('tr'):
+        data = row.findAll('td')
+        if data != []:
+            stateData[data[1].text] = {'Total confirmed cases' : data[2].text, 'Active cases' : data[3].text, 'Cured/Discharged' : data[4].text, 'Deaths' : data[5].text}
+    print(stateData)
+
+
 if __name__ == '__main__':
     print('__Start')
     url = 'https://www.mohfw.gov.in/'
+    url = 'https://prsindia.org/covid-19/cases'
     soup = getSoupObject(url)
-    getGeneralData(soup)
+    getStateData(soup)
     print('__End')
